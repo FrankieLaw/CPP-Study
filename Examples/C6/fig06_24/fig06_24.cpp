729a9f75dba001434ca9522080f7967195874d3b
Frankie Law <frankie.sc.law@hotmail.ca>
2023-01-31 21:01:09 -0500

    Overload Function with Assembly

--- Examples/C6/fig06_24/fig06_24.cpp
+++ Examples/C6/fig06_24/fig06_24.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 using namespace std;
 
+
+// In assembly language "fig06_24.s"
+// Each overloaded function have their own signature based on the name, and parameter listed.
+// _Z6squarei is the integer version of the overloaded function
+// _Z6squared is the double version of the overloaded function
+// _Z6squarec is the char version
+//
+// _Z - is the prefix of overload function
+// 6  - The number 6 is the number of character of the function
+// i  - Is the parameter type.
 int square( int x ) {
     cout << "Square of integer " << x << " is ";
     return x * x;
@@ -13,6 +23,11 @@ double square( double x ) {
     return x * x;
 }
 
+char square( char x ) {
+    cout << "Square of Char " << x << " is ";
+    return '?';
+}
+
 int main( ) {
     cout << endl;
 
@@ -20,6 +35,7 @@ int main( ) {
     cout << endl;
     cout << square( 10.5 );
     cout << endl;
+    cout << square( 'c' );
 
     cout << endl;
 }
---
+++ Examples/C6/fig06_24/fig06_24.s
@@ -0,0 +1,269 @@
+	.file	"fig06_24.cpp"
+	.text
+.lcomm _ZStL8__ioinit,1,1
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Square of integer \0"
+.LC1:
+	.ascii " is \0"
+	.text
+	.globl	_Z6squarei
+	.def	_Z6squarei;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z6squarei
+_Z6squarei:
+.LFB1839:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movl	16(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZNSolsEi
+	movq	%rax, %rcx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	16(%rbp), %eax
+	imull	%eax, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+.LC2:
+	.ascii "Square of double \0"
+	.text
+	.globl	_Z6squared
+	.def	_Z6squared;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z6squared
+_Z6squared:
+.LFB1840:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movsd	%xmm0, 16(%rbp)
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movsd	16(%rbp), %xmm0
+	movapd	%xmm0, %xmm1
+	movq	%rax, %rcx
+	call	_ZNSolsEd
+	movq	%rax, %rcx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movsd	16(%rbp), %xmm0
+	mulsd	%xmm0, %xmm0
+	movq	%xmm0, %rax
+	movq	%rax, %xmm0
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+.LC3:
+	.ascii "Square of Char \0"
+	.text
+	.globl	_Z6squarec
+	.def	_Z6squarec;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z6squarec
+_Z6squarec:
+.LFB1841:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, %eax
+	movb	%al, 16(%rbp)
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movsbl	16(%rbp), %eax
+	movl	%eax, %edx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	movq	%rax, %rcx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$63, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1842:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSolsEPFRSoS_E
+	movl	$7, %ecx
+	call	_Z6squarei
+	movl	%eax, %edx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSolsEi
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSolsEPFRSoS_E
+	movq	.LC4(%rip), %rax
+	movq	%rax, %xmm0
+	call	_Z6squared
+	movq	%xmm0, %rax
+	movq	%rax, %xmm1
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSolsEd
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSolsEPFRSoS_E
+	movl	$99, %ecx
+	call	_Z6squarec
+	movsbl	%al, %eax
+	movl	%eax, %edx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSolsEPFRSoS_E
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB2377:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSt8ios_base4InitD1Ev
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_Z41__static_initialization_and_destruction_0ii
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2376:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	cmpl	$1, 16(%rbp)
+	jne	.L12
+	cmpl	$65535, 24(%rbp)
+	jne	.L12
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rax
+	movq	%rax, %rcx
+	call	atexit
+.L12:
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_GLOBAL__sub_I__Z6squarei;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I__Z6squarei
+_GLOBAL__sub_I__Z6squarei:
+.LFB2378:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	$65535, %edx
+	movl	$1, %ecx
+	call	_Z41__static_initialization_and_destruction_0ii
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z6squarei
+	.section .rdata,"dr"
+	.align 8
+.LC4:
+	.long	0
+	.long	1076166656
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 12.1.0"
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEi;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEd;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	atexit;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, "dr"
+	.globl	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	.linkonce	discard
+.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_:
+	.quad	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
